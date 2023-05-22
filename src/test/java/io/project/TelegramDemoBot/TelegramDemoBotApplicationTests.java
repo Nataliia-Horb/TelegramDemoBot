3acfdc862e3746b1b597fad5b7166804e11e529f
Nataliia Horb <nightangnata@gmail.com>
2023-05-22 19:58:05 +0200

    FirstCommit

---
+++ src/test/java/io/project/TelegramDemoBot/TelegramDemoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.TelegramDemoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramDemoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
